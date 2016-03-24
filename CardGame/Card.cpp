3607c05446e1d0ff9a7f82edad25d163be347f40
lindoh <lindohgamede@gmail.com>
2016-03-24 13:26:31 +0200

    few changes on class declaration and class definition

---
+++ CardGame/Card.cpp
@@ -0,0 +1,27 @@
+#include "Card.h"
+
+using namespace std;
+
+Card::Card(int xValue, string xColor)
+{
+	value = xValue;			//initialising private variable value(number)
+	color = xColor;			//initialising private variable color
+}
+
+Card::~Card()
+{}
+
+int Card::number()
+{
+	return value;		//returns the appropriate number on card
+}
+
+string Card::colour()
+{
+	return color;		//returns appropriate colour of card
+}
+
+void Card::print()
+{
+	cout << colour() << " " << number() << endl;
+}
--- CardGame/Card.h
+++ CardGame/Card.h
@@ -1,6 +1,7 @@
 #ifndef Card_H
 #define Card_H
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -11,7 +12,8 @@ private:
 	string color;		//card colour, Red or Black
 
 public:
-	Card(int xValue, string xColor);		//class Card constructor declaration
+	Card(int, string);		//class Card constructor declaration
+	~Card();				//class Card destructor
 	int number();							//method number() return the number on the card
 	string colour();						//method colour() returns the colour of card (red or black) as string
 	void print();							//method print() displays colour and number of card on screen
